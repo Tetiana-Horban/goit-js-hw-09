90bb5fb1d97ddb7b103b89e0feca6d9adef56abd
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-11-04 23:44:59 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+let timerId = null;
+buttonStop.setAttribute('disabled', true);
+
+buttonStart.addEventListener('click', startChangeBagroundColor);
+buttonStop.addEventListener('click', stopChangeBagroundColor);
+
+function startChangeBagroundColor() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    buttonStart.setAttribute('disabled', true);
+    buttonStop.removeAttribute('disabled');
+  }, 1000);
+}
+function stopChangeBagroundColor() {
+  clearInterval(timerId);
+  buttonStop.setAttribute('disabled', true);
+  buttonStart.removeAttribute('disabled');
+}
+
+buttonStart.classList.add('button');
+buttonStop.classList.add('button');
