4ca27456ba25b2260c0e102536c69ef08bf29bbb
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-11-06 19:20:54 +0200

    task-02

---
+++ src/js/02-timer.js
@@ -0,0 +1,76 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const inputEl = document.querySelector('#datetime-picker');
+const buttonStart = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let startDate = 0;
+
+buttonStart.setAttribute('disabled', true);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    startDate = selectedDates[0];
+    if (new Date() > startDate) {
+      return Notiflix.Notify.failure('Please choose a date in the future');
+    }
+    buttonStart.removeAttribute('disabled');
+  },
+};
+
+flatpickr(inputEl, options);
+buttonStart.addEventListener('click', flatpickr);
+buttonStart.addEventListener('click', startTimer);
+
+function startTimer() {
+  buttonStart.setAttribute('disabled', true);
+  let intervalId = setInterval(() => {
+    const currentDate = Date.now();
+    const deltaDate = startDate - currentDate;
+    const date = convertMs(deltaDate);
+    if (deltaDate >= 0) {
+      updateTimer(date);
+    } else {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimer({ days, hours, minutes, seconds }) {
+  daysEl.textContent = `${days}`;
+  hoursEl.textContent = `${hours}`;
+  minutesEl.textContent = `${minutes}`;
+  secondsEl.textContent = `${seconds}`;
+}
