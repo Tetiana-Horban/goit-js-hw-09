26ee0e05ae7225c1b3aba4eef919f0839f4e8cfa
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-11-07 04:45:19 +0200

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const { delay, step, amount } = event.currentTarget;
+  let delayEl = Number(delay.value);
+  let stepEl = Number(step.value);
+  let amountEl = Number(amount.value);
+
+  for (let position = 1; position <= amountEl; position++) {
+    createPromise(position, delayEl)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayEl += stepEl;
+  }
+});
